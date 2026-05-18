5bb108288e303ad92a6be4a1b2e77de0d2b46ff2
d1ri <d1re.asiri71@gmail.com>
2026-05-18 08:08:46 +0300

    Add Product class with properties and constructor

---
+++ homework6/product.dart
@@ -0,0 +1,14 @@
+class Product {
+  final String name;
+  final String brand;
+  final String description;
+  final int price;
+  final String image;
+
+  Product(
+      {required this.name,
+      required this.brand,
+      required this.description,
+      required this.price,
+      required this.image});
+}
